d317aa03c613d0cf5f75b19faffbc0ad94ca89fa
Murilo Pina <murilo.mpina@gmail.com>
2025-05-12 13:12:43 -0300

    Juntando o while com as funções

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,9 +1,3 @@
-function limparTexto(texto) {
-    return texto;
-}
-function converterNumero(numero) {
-    return Number(numero);
-}
 function estoqueBaixo(quantidade) {
     return quantidade < 5;
 }
@@ -12,24 +6,60 @@ function classificarVinho(idade) {
     if (idade < 15) return "Amadurecido";
     return "Antigo";
 }
-function mostrarDados(usuario, senha, vinho, safra, quantidade, classificacao, estoqueAviso) {
+alert("Bem-vindo(a) á Vinharia Agnello!");
+let anoAtual = 2025;
+let usuario = prompt("Digite o nome do usuário:\n👇 ");
+let senha = prompt("Digite a senha:\n👇 ");
+alert("O usuário e a senha foram cadastrados no console!");
+
+let cadastros = 1;
+var cadastroVinho = prompt("Digite o nome do Vinho que deseja cadastrar:\n👇 ");
+while(cadastroVinho.trim() == "" && cadastroVinho == null) {
+    alert("Digite algo!");
+    cadastroVinho = prompt("Digite o nome do Vinho que deseja cadastrar:\n👇 ");
+}
+alert("Vinho cadastrado com sucesso!");
+var safra = parseInt(prompt("Qual a safra do vinho:\n👇 "));
+var quantidade = parseInt(prompt("Quantos vinhos têm no estoque:\n👇 "));
+var estoqueAviso = estoqueBaixo(quantidade);
+var idade = anoAtual - safra;
+var classificacao = classificarVinho(idade);
+let continuarCadastro = prompt('Quer cadastrar mais algum vinho? (Responda apenas "sim" ou "não"):\n👇 ');
+while(continuarCadastro == "sim" && continuarCadastro == "não") {
+    alert('Digite apenas "sim" ou "não"!');
+    continuarCadastro = prompt('Quer cadastrar mais algum vinho? (Responda "sim" ou "não":\n👇 ');
+}
+while(continuarCadastro == "sim") {
+    cadastroVinho = prompt("Beleza! Digite outro vinho:\n👇 ");
+    cadastros++;
+    alert("Vinho cadastrado com sucesso!");
+    var safra = parseInt(prompt("Qual a safra do vinho:\n👇 "));
+    var quantidade = parseInt(prompt("Quantos vinhos têm no estoque:\n👇 "));
+    var estoqueAviso = estoqueBaixo(quantidade);
+    var idade = anoAtual - safra;
+    var classificacao = classificarVinho(idade);
+    continuarCadastro = prompt('Quer cadastrar mais algum vinho? (Responda "sim" ou "não":\n👇 ');
+    while(continuarCadastro == "sim" && continuarCadastro == "não") {
+        alert('Digite apenas "sim" ou "não"!');
+        continuarCadastro = prompt('Quer cadastrar mais algum vinho? (Responda "sim" ou "não":\n👇 ');
+        mostrarDados(cadastroVinho, safra, quantidade, classificacao, estoqueAviso);
+    }
+}
+if (continuarCadastro == "não") {
+    alert("Obrigado!");
+}
+function mostrarDados(usuario, senha, cadastroVinho, safra, quantidade, classificacao, estoqueAviso, cadastros) {
     console.log("===== Dados Cadastrados =====");
-    console.log("Vinho cadastrado:" + vinho);
     console.log("Usuário Cadastrado: " + usuario);
     console.log("Senha Cadastrada: " + senha);
+    console.log("Vinho cadastrado:" + cadastroVinho);
     console.log("Safra: " + safra);
     console.log("Quantidade: " + quantidade);
     console.log("Classificação: " + classificacao);
     if (estoqueAviso) {
         console.log("Estoque baixo!");
     }
-    alert("Usuário: " + usuario + "\nSenha: " + senha);
-    let vinhoCadastrado = "Vinho cadastrado!"+ vinho + "\n";
-    vinhoCadastrado += "Safra: " + safra + "\n";
-    vinhoCadastrado += "Quantidade: " + quantidade + "\n";
-    vinhoCadastrado += "Classificação: " + classificacao + "\n";
-    if (estoqueAviso) {
-        vinhoCadastrado += "Estoque baixo!";
-    }
-    alert(vinhoCadastrado);
+    console.log(`Número de Cadastros: ${cadastros}`);
 }
+mostrarDados(usuario, senha, cadastroVinho, safra, quantidade, classificacao, estoqueAviso, cadastros);
+mostrarDados(cadastroVinho, safra, quantidade, classificacao, estoqueAviso);
